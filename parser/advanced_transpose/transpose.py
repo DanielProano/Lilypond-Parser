e89f0b2b9ebca10105aec1faf13e1b0300387ee0
Daniel Proano <179722984+DanielProano@users.noreply.github.com>
2025-05-20 10:30:26 -0400

    Up-to-date parser for transposition

--- parser/advanced_transpose/transpose.py
+++ parser/advanced_transpose/transpose.py
@@ -3,6 +3,7 @@ import ly.lex
 from ly.pitch import PitchIterator
 from ly.pitch.transpose import Transposer
 from ly.pitch import Pitch
+from fractions import Fraction
 
 with open("music.ly", "r") as file:
 	content = file.read()
@@ -10,27 +11,19 @@ with open("music.ly", "r") as file:
 s = ly.lex.state("lilypond")
 all_pitches = PitchIterator(s.tokens(content), language='nederlands')
 
-from_pitch = Pitch(0, 0, 1)
-to_pitch = Pitch(1, 0, 1)
+from_pitch = Pitch(0, 0, 0)
+to_pitch = Pitch(0, Fraction(1, 2), 0)
+print(f"from {from_pitch} to_pitch {to_pitch}")
 transposer_custom = Transposer(from_pitch, to_pitch)
 
 last_pitch = None
+happened = False
 with open("output.ly", "w") as file:
-	version, rest_of_file = content.split('\n', 1)
-	file.write(version + "\n\n")
-	file.write(r"absolute {" + "\n")
 	for i in all_pitches.pitches():
-		if isinstance(i, Pitch):
-			transposer_custom.transpose(i)
-			
-			if last_pitch is not None:
-				i.makeAbsolute(last_pitch)
-
-			last_pitch = i.copy()
-			i = i.output('nederlands')
-			file.write(f" {i}")
-			continue
-		else:
+		if not isinstance(i, Pitch):
 			file.write(i)
-	file.write("\n}")
+			continue
 	
+		transposer_custom.transpose(i)
+		out_str = i.output('nederlands')
+		file.write(f" {out_str}")
