885eacc1cdba4bfd2b7ff5ea3861bac003f89521
Daniel Proano <179722984+DanielProano@users.noreply.github.com>
2025-07-21 00:58:58 -0400

    Symmetrical inversion

--- parser/diatonic/output.ly
+++ parser/diatonic/output.ly
@@ -5,13 +5,13 @@
   \key e \major
   \time 4/4
 
-  e 4 e a, a g g a 2
+  e 4 e a' a g g a 2
   b 4 b c c d d e 2
 
-  a, 4 a b b c c d 2
+  a' 4 a b b c c d 2
   a 4 a b b c c d 2
 
-  e 4 e a, a g g a 2
+  e 4 e a' a g g a 2
   b 4 b c c d d e 2
 }
 
--- parser/diatonic/symmetric.py
+++ parser/diatonic/symmetric.py
@@ -8,6 +8,8 @@ def main():
 
 	s = ly.lex.state("lilypond")
 	all_pitches = PitchIterator(s.tokens(content), language='nederlands')
+	
+	axis = Pitch(1, 0, 1)
 
 	with open("output.ly", "w") as file:
 		depth = 0
@@ -37,14 +39,17 @@ def main():
 	
 
 			if depth > 0:
-				inverted_note = 2 * 1 - i.note % 7
-				
-				inverted_alter = -1 * i.alter
+				inverted_note = 2 * axis.note - i.note % 7
 
-				inverted_octave = -1 * i.octave
-				
-				inverted_note = Pitch(inverted_note, inverted_alter, inverted_octave)
-				file.write(inverted_note.output('nederlands') + ' ')
+				inverted_alter = -i.alter
+
+				if i.octave != 0:
+					inverted_octave = 2 * axis.octave - i.octave
+				else:
+					inverted_octave = 0
+	
+				out = Pitch(inverted_note, inverted_alter, inverted_octave)
+				file.write(out.output('nederlands') + ' ')
 			else:
 				file.write(i.output('nederlands'))
 if __name__ == "__main__":
